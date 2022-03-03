604a62522281e775c467151afcbc2a413c1aaaaf
C-Undritz <undritz.cu@gmail.com>
2022-03-03 15:22:02 +0000

    Completed simple query and output of sensor webhook data

--- app.py
+++ app.py
@@ -5,12 +5,35 @@ app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
+    '''
+    Listens for webhook
+    '''
     if request.method == 'POST':
-        print(request.json)
+        data = request.json
+
+        gateway_id = data['uplink_message']['rx_metadata'][0]['gateway_ids']['gateway_id']
+        gateway_eui = data['uplink_message']['rx_metadata'][0]['gateway_ids']['eui']
+        time = data['received_at']
+        temperature = data['uplink_message']['decoded_payload']['temperature']
+
+        print('--------------------------------------')
+        print('Gateway ID: ' + gateway_id)
+        print('Gateway EUI: ' + gateway_eui)
+        print('Time: ' + time)
+        print('Temperature: ' + str(temperature))
+
+        if temperature <= 15:
+            print('It is cold')
+        elif temperature > 15.1 and temperature < 20:
+            print('It is warm')
+        else:
+            print('It is hot')
+
+        print('--------------------------------------')
+
         return 'success', 200
     else:
         abort(400)
-
 
 if __name__ == '__main__':
     app.run()
