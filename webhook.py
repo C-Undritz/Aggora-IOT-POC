3c74f1db0891c3f76929c6f5e9f2acc9f4b05197
C-Undritz <undritz.cu@gmail.com>
2022-04-14 09:14:49 +0000

    changed name of webhook file

--- webhook.py
+++ webhook.py
@@ -10,6 +10,7 @@ def webhook():
     '''
     if request.method == 'POST':
         data = request.json
+        print(data)
 
         gateway_id = data['uplink_message']['rx_metadata'][0]['gateway_ids']['gateway_id']
         gateway_eui = data['uplink_message']['rx_metadata'][0]['gateway_ids']['eui']
