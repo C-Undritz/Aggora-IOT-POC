9d6df896b7d4d0a90b5106c092cf7e8725170185
C-Undritz <undritz.cu@gmail.com>
2022-05-20 08:43:05 +0000

    Added date stamp for recorded ddateime and removed letter from sensor datetime

--- websocket.py
+++ websocket.py
@@ -1,6 +1,7 @@
 import websockets
 import asyncio
 import json
+import datetime
 from modules.hex_to_bytes import decoder
 
 
@@ -14,24 +15,27 @@ async def listen():
         while True:
             data = await uplink.recv()
             json_data = json.loads(data)
-            print(json_data)
             cmd = json_data['cmd']
 
-            if (cmd == 'gw'):
+            if cmd == 'gw':
+                gateway_eui = json_data['gws'][0]['gweui']
                 sensor_eui = json_data['EUI']
                 count = json_data['fcnt']
+                time = json_data['gws'][0]['time']
                 sensor_data = json_data['data']
-                uplink_time = json_data['gws'][0]['time']
-                gateway = json_data['gws'][0]['gweui']
 
+                temperature = decoder(sensor_data)["temperature"]
+                humidity = decoder(sensor_data)["humidity"]
+                uplink_time = time.replace("T", " ").replace("Z", " ")
+                recorded_time = datetime.datetime.now()
+
+                print('Gateway ID: ' + gateway_eui)
                 print('Sensor ID: ' + sensor_eui)
                 print('Uplink Count: ' + str(count))
-                print('Data: ' + sensor_data)
-                environmental_data = decoder(sensor_data)
-                print('temperature: ' + str(environmental_data["temperature"]))
-                print('humidity: ' + str(environmental_data["humidity"]))
-                print('uplink time: ' + str(uplink_time))
-                print('Gateway ID: ' + gateway)
+                print('Uplink Time: ' + str(uplink_time))
+                print('temperature: ' + str(temperature))
+                print('humidity: ' + str(humidity))
+                print('Recorded_time: ' + str(recorded_time))
 
 
 asyncio.get_event_loop().run_until_complete(listen())
